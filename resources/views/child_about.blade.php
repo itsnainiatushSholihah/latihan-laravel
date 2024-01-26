abebb9740fe745987ffa782b100985e0cceec0d6
wecmdn <wec@gmail.com>
2024-01-26 15:12:35 +0700

    latihan dari awal

---
+++ resources/views/child_about.blade.php
@@ -0,0 +1,8 @@
+@extends('home1')
+
+@section('judul', 'home')
+
+@section('content')
+    <h1>INI CONTENT ABOUT</h1>
+    
+@endsection
---
+++ resources/views/child_home.blade.php
@@ -0,0 +1,9 @@
+@extends('home1')
+
+@section('judul', 'home')
+
+@section('content')
+    <h1>INI CONTENT HOME
+    </h1>
+    
+@endsection
